0d3f16f26d1b8cff59d40139868df63a109ec6dc
Toshihiko Doi <36196747+ishot-l@users.noreply.github.com>
2020-02-07 01:31:54 +0900

    postprocessまでやった

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public float maximumZ;
+    public float minimumZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += transform.forward * 0.1f;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position -= transform.forward * 0.1f;
+        }
+
+        if (transform.position.z > maximumZ) {
+            Vector3 pos = transform.position;
+            pos.z = maximumZ;
+            transform.position = pos;
+        } else if (transform.position.z < minimumZ) {
+            Vector3 pos = transform.position;
+            pos.z = minimumZ;
+            transform.position = pos;
+        }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Breakout
+https://qiita.com/nmxi/items/7950fb12ef925efa276d
+<br>を参考に作ったブロック崩し
